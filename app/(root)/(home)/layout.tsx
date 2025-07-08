d7b02d6cc94fdbbe6a3b4281d2f3c41ca9bbb1cc
pivnati <natanmuleta911@gmail.com>
2025-07-08 13:57:20 -0700

    create the layout in the home folder

---
+++ app/(root)/(home)/layout.tsx
@@ -0,0 +1,19 @@
+import React, {ReactNode } from 'react'
+
+const HomeLayout = ({Children}:{Children:ReactNode}) => {
+  return (
+    <main className='relative'>
+       Navbar 
+       <div className='flex'>
+        sidebar
+        <section className='flex min-h-screen flex-1 flex-col px-6 pb-6 pt-28 max-md:pb-14 sm:px-14'>
+            <div className='w-full'>
+                {Children}
+            </div>
+        </section>
+       </div>
+    </main>
+  )
+}
+
+export default HomeLayout
