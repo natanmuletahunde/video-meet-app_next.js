602058c0261fe544f2b54218ca2c2209b53c05b6
pivnati <natanmuleta911@gmail.com>
2025-07-08 21:10:35 -0700

    finsh the mobilenav

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
+import MobileNav from "./MobileNav";
 
 const Navbar = () => {
   return (
@@ -18,7 +19,7 @@ const Navbar = () => {
       </Link>
 
       <div className="flex-between gap-5">
-        MobileNav
+        <MobileNav/>
       </div >
     </nav>
   );
