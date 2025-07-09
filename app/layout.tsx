a60fb75e40163cca1f83bd6192c590084a4290b4
pivnati <natanmuleta911@gmail.com>
2025-07-08 21:28:14 -0700

    create the middleware in the root folder

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,11 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-
-
+import { ClerkProvider } from "@clerk/nextjs";
 
 const inter = Inter({
   subsets: ["latin"],
 });
-
-
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -22,9 +19,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-   <body className={`${inter.className} bg-dark-2`}>
-        {children}
-      </body>
+      <ClerkProvider>
+        <body className={`${inter.className} bg-dark-2`}>{children}</body>
+      </ClerkProvider>
     </html>
   );
 }
