7c61ee57f52622ae1a0b8c1d9c1d177fa27049e4
pivnati <natanmuleta911@gmail.com>
2025-07-09 13:27:07 -0700

    make the middlware add the protocted route

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,8 @@
-import Image from "next/image";
-import Link from "next/link";
-import MobileNav from "./MobileNav";
+import Image from 'next/image';
+import Link from 'next/link';
+import { SignedIn, UserButton } from '@clerk/nextjs';
+
+import MobileNav from './MobileNav';
 
 const Navbar = () => {
   return (
@@ -18,15 +20,14 @@ const Navbar = () => {
         </p>
       </Link>
       <div className="flex-between gap-5">
-        
-      </div>
+        <SignedIn>
+          <UserButton afterSignOutUrl="/sign-in" />
+        </SignedIn>
 
-      <div className="flex-between gap-5">
-        <MobileNav/>
-      </div >
+        <MobileNav />
+      </div>
     </nav>
   );
 };
 
 export default Navbar;
-  // clerk is used for the  
