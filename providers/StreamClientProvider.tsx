7d603ddf80f632d994dc45560e2c87374e3b3bc9
pivnati <natanmuleta911@gmail.com>
2025-07-10 08:49:57 -0700

    create the actions folder and make the stream.actions.ts file inside it

--- providers/StreamClientProvider.tsx
+++ providers/StreamClientProvider.tsx
@@ -1,25 +1,16 @@
 import {
-    StreamCall,
-    StreamVideo,
-    StreamVideoClient,
-    User,
-  } from "@stream-io/video-react-sdk";
-  
-  const apiKey = "your-api-key";
-  const userId = "user-id";
-  const token = "authentication-token";
-  const user: User = { id: userId };
-  
-  const client = new StreamVideoClient({ apiKey, user, token });
-  const call = client.call("default", "my-first-call");
-  call.join({ create: true });
-  
-  export const MyApp = () => {
-    return (
-      <StreamVideo client={client}>
-        <StreamCall call={call}>
-          /* <MyVideoUI /> */
-        </StreamCall>
-      </StreamVideo>
-    );
-  };
+StreamVideoClient,
+  StreamVideo,
+
+} from "@stream-io/video-react-sdk";
+
+const apiKey = "process.env.NEXT_PUBLIC_STREAM_API_KEY"; // Replace with your actual API key
+
+const StreamVideoProvider = ( children) => {
+  return (
+    <StreamVideo client={client}>
+               
+    </StreamVideo>
+  );
+};
+export default StreamVideoProvider;
