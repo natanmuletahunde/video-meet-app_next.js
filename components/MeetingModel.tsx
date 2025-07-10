8218c6cd7afa3b0972cb0f706df2f1178a7aaff1
pivnati <natanmuleta911@gmail.com>
2025-07-10 06:50:41 -0700

    create the meetingmodel

---
+++ components/MeetingModel.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MeetingModel = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default MeetingModel
--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -5,7 +5,12 @@ import HomeCard from "./HomeCard";
 const MeetingTypeList = () => {
   return (
     <section className="grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4">
+     <HomeCard />
      <HomeCard/>
+     <HomeCard/>
+     <HomeCard/>
+
+
     </section>
   );
 };
